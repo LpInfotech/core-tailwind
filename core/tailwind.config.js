8582153e711839cdcf36ae1973829be452dff2e9
Sunil <skumar@constellant.com>
2022-01-12 11:22:08 +0530

    core

--- core/tailwind.config.js
+++ core/tailwind.config.js
@@ -1,6 +1,17 @@
 module.exports = {
   content: ["./public/**/*{.html,js}"],
   theme: {
+    colors:{
+      // primary
+      primary:'#FE8464',
+      // secondary
+      secondary:'#293651',
+    },
+
+    // font family
+    fontFamily:{
+      'open':['Open Sans', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
