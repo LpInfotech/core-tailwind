083ea8828c5c4b210f29cfaa622993cacf50cfe8
Sunil <skumar@constellant.com>
2022-01-17 16:21:01 +0530

    core

--- core/public/main.js
+++ core/public/main.js
@@ -5,3 +5,22 @@ function toggle() {
     dropdown.classList.toggle('hidden');
     menu.classList.add('left-0');
 }
+
+// testimonial slider
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("my-slides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
