e7b8f46674ff088c32a3ed6b9e864445391c38b7
Mukesh <msalaria@constellant.com>
2022-01-19 16:04:06 +0530

    core

--- core/public/main.js
+++ core/public/main.js
@@ -1,6 +1,8 @@
 // refresh page go to top
 $(document).ready(function () {
     $(this).scrollTop(0);
+
+    // by default first tab is active
     $(".tablinks:nth-child(1)").addClass('border-b-2 border-primary text-primary');
     $(".tablinks:nth-child(1)").removeClass('border-b border-secondary_light');
 });
@@ -11,7 +13,7 @@ AOS.init({
 });
 
 // Mobilemenu Navbar.
-function toggle() {
+function togglebtn() {
     var dropdown = document.getElementById('toggleMenu');
     var menu = document.getElementById('dropdownMenu');
     dropdown.classList.toggle('hidden');
@@ -37,13 +39,13 @@ $(window).scroll(function () {
         var sectionOffset = $(this.hash).offset().top - 73;
         if (sectionOffset <= scrollbarLocation) {
             $(this).parent().addClass('border-b-2 border-primary');
-            $(this).parent().siblings().removeClass('border-b-2 border-primary');
+            $(this).parent().siblings().removeClass('border-b-2 border-primary dark:border-secondary');
         }
     });
 });
 
 // technology tabs start
-function openCity(evt, cityName) {
+function openCity(evt, tabName) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
@@ -53,17 +55,17 @@ function openCity(evt, cityName) {
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace("active", "");
     }
-    document.getElementById(cityName).style.display = "block";
+    document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
 }
- // defaultopen
- document.getElementById("defaultOpen").click();
+// defaultopen
+document.getElementById("defaultOpen").click();
 
 // technology button active
-$('.tablinks').click(function(){
+$('.tablinks').click(function () {
     $('.tablinks').removeClass('border-b-2 border-primary text-primary');
     $('.tablinks').addClass('border-b border-secondary_light');
-    if($(this).find('active')){
+    if ($(this).find('active')) {
         $(this).addClass('border-b-2 border-primary text-primary');
         $(this).removeClass('border-b border-secondary_light');
     }
@@ -92,15 +94,38 @@ function showDivs(n) {
     x[slideIndex - 1].style.display = "block";
 }
 
-// switch to darl mode
+// switch to dark mode mobile
+function onchangeTheme() {
+    var i;
+    var checkbox = document.getElementsByClassName('toggle')
+    var html = document.querySelector('html');
+    for (i = 0; i < checkbox.length; i++) {
+        checkbox[i].checked ?
+            html.classList.add("dark") :
+            html.classList.remove("dark")
+    }
+}
+
+// switch to dark mode desktop
 const checkbox = document.querySelector('#toggle');
 const html = document.querySelector('html');
 
-const toggleDarkMode = function (){
+const toggleDarkMode = function () {
     checkbox.checked
-    ? html.classList.add("dark")
-    : html.classList.remove("dark")
+        ? html.classList.add("dark")
+        : html.classList.remove("dark")
 }
 
 toggleDarkMode();
 checkbox.addEventListener("click", toggleDarkMode);
+
+// onclick light and dark svg icons change
+$('input[type="checkbox"]').click(function () {
+    if ($(this).prop("checked") == true) {
+        $(".sun-svg").addClass('hidden');
+        $(".moon-svg").removeClass('hidden');
+    } else if ($(this).prop("checked") == false) {
+        $(".moon-svg").addClass('hidden');
+        $(".sun-svg").removeClass('hidden');
+    }
+});
